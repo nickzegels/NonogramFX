46f06a286c059d92fd29ded61a605e3a142fbb3e
nickzegels <58815625+nickzegels@users.noreply.github.com>
2020-03-10 10:34:46 +0100

    Add files via upload

--- GameView.java
+++ GameView.java
@@ -6,12 +6,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 public class GameView extends GridPane {
-    //
-
     private Image foto;
     private Background achtergrond;
     private BackgroundSize achtergrondgrootte;
@@ -20,54 +19,47 @@ public class GameView extends GridPane {
     private Button testButton;
     private GridPane rooster;
 
-
     public GameView() {
         this.initialiseNodes();
         this.layoutNodes();
     }
 
     private void initialiseNodes() {
-        //this.foto = new Image("/background.jpg");
         this.testButton = new Button("");
         this.rooster = new GridPane();
+    }
 
+    private void cellLayout(Label cell) {
+        cell.setText("Cell");
+        cell.setPrefWidth(100);
+        cell.setPrefHeight(100);
     }
 
     private void layoutNodes() {
-        //this.achtergrondgrootte = new BackgroundSize(0, 0, true, true, true, true);
-        //this.setBackground(new Background(new BackgroundImage(foto, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, achtergrondgrootte)));
-
         //Grid
         this.setGridLinesVisible(true);
-        this.setWidth(300);
-        //this.minWidth(250);
-        this.maxHeight(300);
+        rooster.setGridLinesVisible(true);
+
+        for (int a = 0; a < 7; a++) {
+            for (int b = 0; b < 7; b++) {
+                Label cell = new Label("ha");
+                cellLayout(cell);
+                rooster.add(cell, a, b, 1, 1);
+            }
+        }
+        this.add(rooster, 1, 1);
 
         this.setAlignment(Pos.CENTER);
 
-        ColumnConstraints column1 = new ColumnConstraints(350);
-        ColumnConstraints column2 = new ColumnConstraints(350);
-        this.getColumnConstraints().addAll(column1, column2);
+        ColumnConstraints column1 = new ColumnConstraints(50);
+        ColumnConstraints column2 = new ColumnConstraints(480);
+        ColumnConstraints column3 = new ColumnConstraints(125);
+        this.getColumnConstraints().addAll(column1, column2, column3);
 
 
-        RowConstraints rowConstraints = new RowConstraints(350);
-        RowConstraints rowConstraints2 = new RowConstraints(350);
-        this.getRowConstraints().addAll(rowConstraints, rowConstraints2);
-
-        
-
-
-//        for (int i = 0; i<10;i++) {
-//            for (int j = 0; j<10; j++) {
-//                //this.getColumnConstraints().addAll(column1);
-//                //this.getRowConstraints().addAll(rowConstraints);
-//                //Button
-//                this.add(testButton, i, j);
-//                this.testButton.setPrefSize(50, 50);
-//            }
-//        }
-
-
+        RowConstraints rowConstraints = new RowConstraints(150);
+        RowConstraints rowConstraints2 = new RowConstraints(480);
+        RowConstraints rowConstraints3 = new RowConstraints(50);
+        this.getRowConstraints().addAll(rowConstraints, rowConstraints2, rowConstraints3);
     }
-
 }
